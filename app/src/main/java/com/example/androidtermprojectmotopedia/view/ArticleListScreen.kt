a275d6a3d002a7b878f8c87c1cb39b003eb2cca8
haoz20 <zhaonianhao2005@gmail.com>
2025-03-01 14:28:14 +0700

    Add sorting feature in home screen

--- app/src/main/java/com/example/androidtermprojectmotopedia/view/ArticleListScreen.kt
+++ app/src/main/java/com/example/androidtermprojectmotopedia/view/ArticleListScreen.kt
@@ -1,11 +1,9 @@
 package com.example.androidtermprojectmotopedia.view
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,7 +15,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -27,11 +24,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import com.example.androidtermprojectmotopedia.ui.backgrounds.WaveBackground
 import com.example.androidtermprojectmotopedia.viewModel.MotorcycleViewModel
@@ -47,7 +42,7 @@ fun ArticleListScreen(
     val errorMessage by motorcycleViewModel.errorMessage.collectAsState()
     val approvedMotorcycles = motorcycles.filter { it.status != "pending" }
     val groupedByBrand = remember(approvedMotorcycles) {
-        approvedMotorcycles.groupBy { it.brand }
+        approvedMotorcycles.groupBy { it.brand }.toSortedMap()
     }
 
     if (errorMessage != null) {
